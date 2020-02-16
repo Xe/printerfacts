5e2980401291cd970730bf7ee42209ffdeb08968
Christine Dodrill <me@christine.website>
2020-02-16 13:24:18 -0500

    Update deploy.sh

--- nix/deploy.sh
+++ nix/deploy.sh
@@ -7,5 +7,5 @@ docker tag xena/printerfacts:latest xena/printerfacts:$GITHUB_SHA
 docker push xena/printerfacts:$GITHUB_SHA
 dhall-to-yaml-ng --omit-empty --file printerfacts.dhall > $GITHUB_WORKSPACE/deploy.yml
 doctl kubernetes cluster kubeconfig show kubermemes > $GITHUB_WORKSPACE/.kubeconfig
-kubectl --kubeconfig=/github/workspace/.kubeconfig apply -n apps -f /github/workspace/deploy.yml
+kubectl --kubeconfig=$GITHUB_WORKSPACE/.kubeconfig apply -n apps -f $GITHUB_WORKSPACE/deploy.yml
 
